72c888d770ddaac65240ed82f150223d44343324
NikOkol <79254321+NikOkol@users.noreply.github.com>
2021-04-04 14:01:02 +0600

    Update lab6.cpp

--- lab6.cpp
+++ lab6.cpp
@@ -1,60 +1,123 @@
-﻿
+
 #include <iostream>
+#include <string>
 using namespace std;
+
+int scan_natural_number();
+
+struct second_list
+{
+    int age;
+    second_list* next_second;
+};
 
 struct first_list
 {
-    int m;
-    first_list* p;
+    string name;
+    first_list* next_first;
+    second_list* to_second;
 };
 
-void getm(int k, first_list* q)
-{
-    int i;
-    first_list* t, * t1;
-    t = q;
-    for (i = 1; i < k; i++)
-    {
-        t1 = t->p;
-        t = t1;
-    }
-    cout << "\nValue is " << t->m;
-}
+
+
+void list_output(first_list*, int);
 
 int main()
 {
-    int i, k, n;
-    first_list *q0, *q1, *q2;
-    q0 = new first_list;
-    q1 = q0;
-    cout << "\nEnter n: ";
-    cin >> n;
-    for (i = 1; i < n; i++)
+    setlocale(LC_ALL, "Russian");
+    int n = 0;
+    string string_input;
+    first_list* first_f, * current_f, * last_f;
+    second_list* first_s, * current_s, * last_s;
+    first_f = new first_list;
+    first_s = new second_list;
+    current_f = first_f;
+    current_s = first_s;
+    last_s = nullptr;
+    last_f = nullptr;
+    cout << "\nЗаполните список имён людей и их возраста(в полных годах). Для завершения введите 'end' ";
+    do
     {
-        cout << "\nValue m = ";
-        cin >> q1->m;
-        q2 = new first_list;
-        q1->p = q2;
-        q1 = q2;
-    }
-    cout << "\nValue m = ";
-    cin >> q1->m;
-    q1->p = q0;
-    do 
-    {
-        cout << "\nValue for index k = ";
-        cin >> k;
-        if (!k)
+        cout << "\n " << n + 1 << ")  Имя: ";
+        getline(cin, string_input);
+        if (string_input == "end")
         {
-            for (i = 1; i <= n; i++)
-            {
-                q1 = q2->p;
-                delete q2;
-                q2 = q1;
-            }
-            return 0;
+            break;
         }
-        getm(k, q0);
+        n++;
+        current_f->to_second = current_s;
+        current_f->name = string_input;
+        cout << "  Возраст: ";
+        current_s->age = scan_natural_number();
+        last_f = new first_list;
+        current_f->next_first = last_f;
+        last_s = new second_list;
+        current_s->next_second = last_s;
+        current_f = last_f;
+        current_s = last_s;
+        cin.ignore(32767, '\n');
     } while (true);
+
+    current_f->next_first = first_f;
+    current_s->next_second = first_s;
+    list_output(first_f, n);
+
+    for (int i = 0; i <= n; i++)
+    {
+        current_f = last_f->next_first;
+        delete last_f;
+        last_f = current_f;
+        current_s = last_s->next_second;
+        delete last_s;
+        last_s = current_s;
+    }
 }
 
+void list_output(first_list* first_f, int n)
+{
+    first_list* cur, * next;
+    cur = first_f;
+    second_list* cur_s;
+    for (int i = 0; i < n; i++)
+    {
+        next = cur->next_first;
+        cur_s = cur->to_second;
+        for (int j = 0; j < (17 - (cur->name.length())); j++)
+        {
+            cout << "---";
+        }
+        cout << "\n" << i + 1 << ")   Имя: " << cur->name;
+        for (int j = 0; j < (10- (cur->name.length())); j++)
+        {
+            cout << " ";
+        }
+        cout << " | Возраст: " << cur_s->age << "\n";
+        for (int j = 0; j < (17 - (cur->name.length())); j++)
+        {
+            cout << "---";
+        }
+        cout << "\n";
+        cur = next;
+    }
+}
+
+
+int scan_natural_number() 
+{
+    while (true)
+    {
+        int input_data;
+        cin >> input_data;
+
+        if (cin.fail() || input_data <= 0) 
+        {
+            cout << "\nВведено неверное значение. Повторите попытку: ";
+            cin.clear();
+            cin.ignore(32767, '\n');
+        }
+        else
+        {
+            return input_data;
+        }
+    }
+}
