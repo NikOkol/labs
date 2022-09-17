6ac299d94044d3af8f62670d19add6e4b86e039f
NikOkol <79254321+NikOkol@users.noreply.github.com>
2022-09-17 15:35:26 +0600

    Add files via upload

--- CryptMethodsLab1/Program.cs
+++ CryptMethodsLab1/Program.cs
@@ -7,7 +7,8 @@ namespace CryptMethodsLab1
 {
     class Program
     {
-        static void Main(string[] args)
+        
+        static void Main(string[] args) // Меню для выбора действия
         {
             while (true)
             {
@@ -19,13 +20,41 @@ namespace CryptMethodsLab1
                 {
                     case 0:
                         return;
-                    case 1:
-                        Encryption();
+
+                    case 1:      // Выбор шифрования
+                        Console.WriteLine("Enter file path to encrypt: ");
+                        string FilePath = PathInput();
+                        Console.WriteLine("Enter new file path or '0' if you want to overwrite original file: ");
+                        string EditedFilePath = NotNullStringInput();
+
+                        if (EditedFilePath == "0")
+                        {
+                            FileEncryption(FilePath);
+                        }
+                        else
+                        {
+                            FileEncryption(FilePath, EditedFilePath);
+                        }
                         Console.WriteLine("File was encrypted");
                         break;
-                    case 2:
-                        Decryption();
+
+                    case 2:     // Выбор дешифровки
+                        Console.WriteLine("Enter file path to decrypt: ");
+                        string EncryptedFilePath = PathInput();
+                        Console.WriteLine("Enter new file path or '0' if you want to overwrite original file: ");
+                        string DecryptingFilePath = NotNullStringInput();
+
+                        if (DecryptingFilePath == "0")
+                        {
+                            FileDecryption(EncryptedFilePath);
+                        }
+                        else
+                        {
+                            FileDecryption(EncryptedFilePath, DecryptingFilePath);
+                        }
+                        Console.WriteLine("File was decrypted");
                         break;
+
                     default:
                         Console.WriteLine("Incorrect value");
                         break;
@@ -33,75 +62,103 @@ namespace CryptMethodsLab1
             }
         }
 
-        static void Encryption()
+        
+
+        static void FileEncryption(string FilePath) // Шифрование файла с перезаписью оригинала
         {
-            List<string> ListOfLines = new List<string>();
-
-            StreamReader sr = new StreamReader("D:\\Important_Information.txt");
-            
-
-            string line = sr.ReadLine();
-            char[] CharArray = line.ToCharArray();
-            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
-            byte[] bytes = Encoding.Latin1.GetBytes(line);
-
-            while (line != null)
+            byte[] bytes = File.ReadAllBytes(FilePath);
+            int i = 0;
+            foreach (byte b in bytes)
             {
-                CharArray = line.ToCharArray();
-                bytes = Encoding.Latin1.GetBytes(line);
-                for (int i = 0; i < line.Length; i++)
-                {
-                    CharArray[i] = Convert.ToChar((bytes[i] * 5 + 23) % 256);
-                }
-                line = new string(CharArray);
-                ListOfLines.Add(line);
-                line = sr.ReadLine();
-            }
-            sr.Close();
-
-            StreamWriter sw = new StreamWriter("D:\\Cipher.txt");
-            foreach (string Line in ListOfLines)
-            {
-                sw.WriteLine(Line);
+                bytes[i] = (byte)((bytes[i] * 5 + 23) % 256);
+                i++;
             }
 
-            sw.Close();
+            File.WriteAllBytes(FilePath, bytes);
         }
 
-        static void Decryption()
+        static void FileEncryption(string FilePath, string EditedFilePath) // Шифрование файла с созданием зашифрованной копии
         {
-            List<string> ListOfLines = new List<string>();
-
-            StreamReader sr = new StreamReader("D:\\Cipher.txt");
-
-            string line = sr.ReadLine();
-            char[] CharArray = line.ToCharArray();
-            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
-            byte[] bytes = Encoding.Latin1.GetBytes(line);
-
-            while (line != null)
+            byte[] bytes = File.ReadAllBytes(FilePath);
+            int i = 0;
+            foreach (byte b in bytes)
             {
-                CharArray = line.ToCharArray();
-                bytes = Encoding.Latin1.GetBytes(line);
-                for (int i = 0; i < line.Length; i++)
-                {
-                    CharArray[i] = Convert.ToChar(((bytes[i] - 23 + 256) * 205) % 256);
-                }
-                line = new string(CharArray);
-                ListOfLines.Add(line);
-                line = sr.ReadLine();
+                bytes[i] = (byte)((bytes[i] * 5 + 23) % 256);
+                i++;
             }
-            sr.Close();
 
-            StreamWriter sw = new StreamWriter("D:\\Decrypted.txt");
-            foreach(string Line in ListOfLines)
+            File.WriteAllBytes(EditedFilePath, bytes);
+        }
+        
+        static void FileDecryption(string FilePath) // Расшифрование файла с перезаписью оригинала
+        {
+            byte[] bytes = File.ReadAllBytes(FilePath);
+            int i = 0;
+            foreach (byte b in bytes)
             {
-                sw.WriteLine(Line);
+                bytes[i] = (byte)(((bytes[i] - 23 + 256) * ReciprocalNumber(5, 256)) % 256);
+                i++;
             }
-            sw.Close();
+
+            File.WriteAllBytes(FilePath, bytes);
         }
 
-        public static int Int_input()
+        static void FileDecryption(string FilePath, string DecryptedFilePath) // Расшифрование файла с созданием расшифрованной копии
+        {
+            byte[] bytes = File.ReadAllBytes(FilePath);
+            int i = 0;
+            foreach (byte b in bytes)
+            {
+                bytes[i] = (byte)(((bytes[i] - 23 + 256) * ReciprocalNumber(5, 256)) % 256);
+                i++;
+            }
+
+            File.WriteAllBytes(DecryptedFilePath, bytes);
+        }
+
+        static int ReciprocalNumber(int a, int m) // Поиск обратного элемента по модулю m
+        {
+            int x = 1;
+            while(((a * x)% m) != 1)
+            {
+                x++;
+                if(x > m)
+                {
+                    Console.WriteLine("Error: Reciprosal does not exist");
+                    return 0;
+                }
+            }
+            return x;
+        }
+
+        static string NotNullStringInput()
+        {
+            string input = Console.ReadLine();
+            while(input == "")
+            {
+                Console.WriteLine("Enter valid value");
+                input = Console.ReadLine();
+            }
+            return input;
+        }
+
+        static string PathInput() // Проверка на существование файла
+        {
+            while(true)
+            {
+                string input = Console.ReadLine();
+                if(File.Exists(input))
+                {
+                    return input;
+                }
+                else
+                {
+                    Console.WriteLine("File does not exist. Try again");
+                }
+            }
+        }
+
+        public static int Int_input() // Чтение integer
         {
             while (true)
             {
