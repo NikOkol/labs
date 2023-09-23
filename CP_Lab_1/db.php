c4d0294ce55208fc40c99a24101ff49b4385bb62
NikOkol <79254321+NikOkol@users.noreply.github.com>
2023-09-23 14:23:03 +0600

    Delete CP_Lab_1 directory

--- CP_Lab_1/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$servername = "localhost";
-$username = "root";
-$password = "root";
-
-$conn_users = mysqli_connect($servername, $username, $password, "users");
-$conn_messages = mysqli_connect($servername, $username, $password, "messages");
-?>
